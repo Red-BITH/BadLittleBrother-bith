ee94762cdaaba811af2bcf0d18a3c97bd8cba1f3
Red-BITH <136892734+Red-BITH@users.noreply.github.com>
2023-08-09 01:01:42 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,15 @@
+print("\033[91mBOOT: v0.0.1 EARLY ACCESS\033[0m")
+import time
+import os
+time.sleep(1)
+print("""\033[92mCHOOSE LANGUAGE:
+\033[0m""")
+print("\033[94m 1. ENGLİSH/US\033[0m")
+print("\033[92m 2. Azərbaycan\Aze\033[0m")
+a = input("\033[0mEnter\Daxil et\033[0m")
 
+if(a=="1"):
+
+if(a=="2"):
+  
+  
